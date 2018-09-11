ae4b41cd02307b562d85b6c7c1a8d1ade3412b1e
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-09-11 10:09:36 -0400

    Added some param comments in the ENtity header

--- HW3_Moving/HW3_Moving/Entity.h
+++ HW3_Moving/HW3_Moving/Entity.h
@@ -15,8 +15,9 @@ class Entity
 {
 
 public:
+
 	/** 
-	* 
+	* Constructor for this entity
 	* 
 	* 
 	* @param aMesh	A pointer to this entity's mesh
@@ -25,7 +26,6 @@ public:
 	
 	/** Virtual destructor for this mesh */
 	virtual ~Entity();
-
 
 	////////////////////////////////////////////////////
 	// Accessors
@@ -37,25 +37,47 @@ public:
 	/** Return's this entity's current position */
 	const DirectX::XMFLOAT3 & GetPosition() const;
 
-	/** Set the position of this entity */
+	/** Set the position of this entity
+	*
+	* @param aNewPos	The new position of this object
+	*/
 	void SetPosition(const DirectX::XMFLOAT3 & aNewPos);
 
-	/** Set the positoin of this enetiy */
+	/** Set the position of this enetiy 
+	*
+	* @param aX		new X value of the position
+	* @param aY		new Y value of the position
+	* @param aZ		new Z value of the position
+	*/
 	void SetPosition(const float aX, const float aY, const float aZ);
 
 	/** Return's this entity's current scale */
 	const DirectX::XMFLOAT3 & GetScale() const;
 
-	/** Set the position of this entity */
+	/**
+	* Set the position of this entity 
+	* 
+	* @param aNewScale	The new scale of this object
+	*/
 	void SetScale(const DirectX::XMFLOAT3 & aNewScale);
 
-	/** Set the scale of the this entity */
+	/** 
+	* Set the scale of the this entity 
+	*
+	* @param aX		new X value of the scale
+	* @param aY		new Y value of the scale
+	* @param aZ		new Z value of the scale
+	*/
 	void SetScale(const float aX, const float aY, const float aZ);
 
 	/** Return's this entity's current rotation */
 	const DirectX::XMFLOAT4X4 & GetRotation() const;
 
-	/** Set the rotation of this entity */
+	/** 
+	* Set the rotation of this entity 
+	* 
+	* @param aNewRot	The new rotation of this object
+	*/
 	void SetRotation(const DirectX::XMFLOAT4X4 & aNewRot);
 
 private:
