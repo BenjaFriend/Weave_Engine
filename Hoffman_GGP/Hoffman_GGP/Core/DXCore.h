b3e773927ac1436856b6b0bf6d43f1c911f2fd36
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-11-11 09:43:24 -0500

    Move all external libs to the additional folder

--- Hoffman_GGP/Hoffman_GGP/Core/DXCore.h
+++ Hoffman_GGP/Hoffman_GGP/Core/DXCore.h
@@ -11,9 +11,9 @@
 
 #if defined(ENABLE_UI)
 
-#include "../Utils/imgui/imgui.h"
-#include "../Utils/imgui/imgui_impl_win32.h"
-#include "../Utils/imgui/imgui_impl_dx11.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_win32.h"
+#include "imgui/imgui_impl_dx11.h"
 
 #endif
 
