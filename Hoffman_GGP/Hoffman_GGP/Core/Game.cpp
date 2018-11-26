8e04685fd9092eecce8cc1bb39cfd324fb1bfcfa
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-11-26 10:05:41 -0500

    Play with AA settings

--- Hoffman_GGP/Hoffman_GGP/Core/Game.cpp
+++ Hoffman_GGP/Hoffman_GGP/Core/Game.cpp
@@ -193,6 +193,7 @@ void Game::CreateBasicGeometry()
     samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
     samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
     samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+    //samplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
     samplerDesc.MaxAnisotropy = 16;
     samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
 
