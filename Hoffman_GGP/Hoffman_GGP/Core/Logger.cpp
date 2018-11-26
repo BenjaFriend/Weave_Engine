57e0cc6207d8bb1258dc018c2790be9ae62a1118
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-11-25 19:00:04 -0500

    edit log settings

--- Hoffman_GGP/Hoffman_GGP/Core/Logger.cpp
+++ Hoffman_GGP/Hoffman_GGP/Core/Logger.cpp
@@ -4,7 +4,7 @@
 
 Logger::Logger()
 {
-   /* spdlog::info( "Welcome to spdlog version {}.{}.{} !", 
+   /*spdlog::info( "Welcome to spdlog version {}.{}.{} !", 
         SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH );*/
 }
 
