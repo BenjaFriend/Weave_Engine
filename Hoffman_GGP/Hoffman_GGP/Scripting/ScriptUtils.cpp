4dfeb501cd36e9c2767e230f6d8f6c7b0f28852b
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2018-12-04 20:39:19 -0500

    Edit the creation of entity data

--- Hoffman_GGP/Hoffman_GGP/Scripting/ScriptUtils.cpp
+++ Hoffman_GGP/Hoffman_GGP/Scripting/ScriptUtils.cpp
@@ -7,15 +7,25 @@ void Scripting::TestScripting()
     sol::state lua;
     lua.open_libraries( sol::lib::base );
 
+    struct EntityCreationData
+    {
+        int data;
+        std::string file;
+    };
+
+    lua.new_usertype<EntityCreationData>( "EntityCreationData" );
+
+
     lua.script_file( "Assets/Scripts/test.lua" );
 
     sol::optional<sol::table> maybeObjs = lua[ "objs" ];
     if ( maybeObjs != sol::nullopt )
     {
         sol::table& objsTable = maybeObjs.value();
-       
 
-        auto parseFunc = [ ] ( auto& func, sol::table & aTable ) -> void
+        std::vector<std::reference_wrapper<EntityCreationData>> entitiesToCreate;
+
+        auto parseFunc = [ &entitiesToCreate ] ( auto& func, sol::table & aTable ) -> void
         {
             // Iterate over this table
             for ( auto key_val : aTable )
@@ -28,21 +38,34 @@ void Scripting::TestScripting()
                 // Determine the type of data that is in this key
                 auto t = value.get_type();
                 switch ( t )
-                {             
+                {
                 case sol::type::table:
                     // Check for other components types
                     func( func, value.as<sol::table>() );
-
                     break;
+
                 case sol::type::string:
                     // What is this key? 
                     // Do something with that value
                     //aTable[ key.as<std::string>() ];
                     break;
-                //case sol::type::number:
-                //case sol::type::boolean:
-                //case sol::type::function:
-                //case sol::type::userdata:
+                    //case sol::type::number:
+                    //case sol::type::boolean:
+                    //case sol::type::function:
+                case sol::type::userdata:
+
+                    //sol::optional<EntityCreationData&> possibleEntity = sol::nullopt;
+
+                    //possibleEntity = value.as<sol::optional<EntityCreationData&>>();
+                    //if ( possibleEntity )
+                    //{
+                       // EntityCreationData& aEntity = possibleEntity.value();
+                        //entitiesToCreate.push_back( aEntity );
+                    //}
+
+
+                    break;
+                    
                 case sol::type::none:
                 case sol::type::lua_nil:
                 default:
@@ -52,15 +75,11 @@ void Scripting::TestScripting()
         };
 
         parseFunc( parseFunc, objsTable );
-        
+
 
     }
 }
 
-void Scripting::ParseTable( sol::table & aTable )
-{
-    
-}
 
 /// <summary>
 ///  https://github.com/ThePhD/sol2/blob/develop/examples/optional_with_iteration.cpp
