e8310bcb816b19f0073dcf3c04af5fd4ad891018
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2019-01-24 16:36:04 -0500

    Fix camera relative movement

--- Weave_Engine/Weave_Engine/Entity/Camera.cpp
+++ Weave_Engine/Weave_Engine/Entity/Camera.cpp
@@ -24,22 +24,27 @@ Camera::~Camera()
 void Camera::Update( const float aDeltaTime )
 {
     RelativeInput.x = 0.f;
-    RelativeInput.z = 0.f;
     RelativeInput.y = 0.f;
+    RelativeInput.z = 0.f;
 
+    // Forward and back
     if ( inputManager->IsKeyDown( 'W' ) ) { RelativeInput.z += 1.f; }
     if ( inputManager->IsKeyDown( 'S' ) ) { RelativeInput.z -= 1.f; }
-    if ( inputManager->IsKeyDown( 'A' ) ) { RelativeInput.x -= 1.f; }
-    if ( inputManager->IsKeyDown( 'D' ) ) { RelativeInput.x += 1.f; }
+    
+    // Left/Right
+    if ( inputManager->IsKeyDown( 'A' ) ) { RelativeInput.x += 1.f; }
+    if ( inputManager->IsKeyDown( 'D' ) ) { RelativeInput.x -= 1.f; }
 
-    // Moving up and down (Absolute)
-    if ( GetAsyncKeyState( 'X' ) & 0x80000 ) { RelativeInput.y -= 1.f; }
-    if ( GetAsyncKeyState( VK_SPACE ) & 0x80000 ) { RelativeInput.y += 1.f; }
+    // Up/Down
+    if ( GetAsyncKeyState( 'Q' ) & 0x80000 ) { RelativeInput.y -= 1.f; }
+    if ( GetAsyncKeyState( 'E' ) & 0x80000 ) { RelativeInput.y += 1.f; }
 
     // Scale keyboard input by delta time
     RelativeInput *= aDeltaTime * MovementSpeed;
 
-    Pos += RelativeInput;
+    Pos += Right * RelativeInput.x;
+    Pos += Up * RelativeInput.y;
+    Pos += Forward * RelativeInput.z;
 }
 
 void Camera::SetDoRotation( bool aDoRot )
