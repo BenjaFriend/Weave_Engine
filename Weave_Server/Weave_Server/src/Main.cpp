e03a699560a049524cbdc28e1633fd61cd1d9cf9
Ben Hoffman <bah8892@rit.edu>
2019-03-04 12:48:10 -0500

    Added lambda test

--- Weave_Server/Weave_Server/src/Main.cpp
+++ Weave_Server/Weave_Server/src/Main.cpp
@@ -1,9 +1,15 @@
 #include "Test_Include.h"
 #include "Funcs.h"
+#include <boost/lambda/lambda.hpp>
 #include <boost/asio.hpp>
 
 int main( int argc, char* argv[] )
 {
+    if( argc <= 1 )
+    {
+        std::cout << "There is only one argument at argv[0]"
+        << argv[ 0 ] << std::endl;
+    }
     std::cout << "Hello world!" << std::endl;
     TestFunc();
     return 0;
