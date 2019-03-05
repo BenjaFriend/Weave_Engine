d5278188ae12d664b4ceebde40070ff94977daf7
Ben Hoffman <bah8892@rit.edu>
2019-03-05 12:38:53 -0500

    Update the cmake file to properly link boost on ubuntu

--- Weave_Server/Weave_Server/src/Main.cpp
+++ Weave_Server/Weave_Server/src/Main.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <string>
+#include <thread>
+
 #include "Test_Include.h"
 #include "Funcs.h"
+
 #include <boost/lambda/lambda.hpp>
 #include <boost/asio.hpp>
+#include <boost/regex.hpp>
 
 int main( int argc, char* argv[] )
 {
@@ -10,7 +18,7 @@ int main( int argc, char* argv[] )
         std::cout << "There is only one argument at argv[0]"
         << argv[ 0 ] << std::endl;
     }
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "Hello world! Initalize asio and boost please!" << std::endl;
     TestFunc();
     return 0;
 }
